cf62e8872c3458c7937d2a2236010f689d1a3a38
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-09-18 14:03:39 -0300

    feat: check if token belongs to existing user implemented

--- src/middlewares/tokenMiddleware.ts
+++ src/middlewares/tokenMiddleware.ts
@@ -1,22 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import * as userService from "../services/userService";
 
-export async function tokenValidationMiddleware(req:Request, res: Response, next: NextFunction) {
-    const authorization = req.headers.authorization;
-    const token = authorization?.replace("Bearer ", "").trim();
+export async function tokenValidationMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authorization = req.headers.authorization;
+  const token = authorization?.replace("Bearer ", "").trim();
 
-    type MyToken = {
-        id: string
-        iat: number
-      }
+  type MyToken = {
+    id: string;
+    iat: number;
+  };
 
-    if(!token){
-        throw {code: 'Unauthorized', message: 'Token must been sent'}
-    }
+  if (!token) {
+    throw { code: "Unauthorized", message: "Token must been sent" };
+  }
 
-        const secretKey:string | undefined= process.env.JWT_SECRET;
-        const verify : string|JwtPayload  = jwt.verify(token, secretKey!) as MyToken;
-        res.locals.userId= verify.id;
-        next();
-  
+  const secretKey: string | undefined = process.env.JWT_SECRET;
+  const verify: string | JwtPayload = jwt.verify(token, secretKey!) as MyToken;
+  await userService.findById(verify.id);
+  res.locals.userId = verify.id;
+  next();
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,39 +1,47 @@
 import * as userRepository from "../repositories/userRepository";
 import { UserData } from "../types/userType";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-async function createUser(userData: UserData){
-    const existingEmail = await userRepository.findByEmail(userData.email);
-    if(existingEmail){
-        throw {code: "Conflict", message: "Email already registered"}
-    }
+async function createUser(userData: UserData) {
+  const existingEmail = await userRepository.findByEmail(userData.email);
+  if (existingEmail) {
+    throw { code: "Conflict", message: "Email already registered" };
+  }
 
-    const passwordEncrypted = bcrypt.hashSync(userData.password, 10);
-    const user:UserData = {
-        email: userData.email,
-        password: passwordEncrypted
-    }
-    await userRepository.createUser(user);
+  const passwordEncrypted = bcrypt.hashSync(userData.password, 10);
+  const user: UserData = {
+    email: userData.email,
+    password: passwordEncrypted,
+  };
+  await userRepository.createUser(user);
 }
 
-async function signIn(userData: UserData){
-    const existingEmail = await userRepository.findByEmail(userData.email);
-    if(!existingEmail){
-        throw { code: "Unauthorized", message: "Incorrect e-mail and/or password"}
-    }
-    const checkPassword = bcrypt.compareSync(userData.password, existingEmail.password);
-    if(!checkPassword){
-        throw { code: "Unauthorized", message: "Incorrect e-mail and/or password"}
-    }
+async function signIn(userData: UserData) {
+  const existingEmail = await userRepository.findByEmail(userData.email);
+  if (!existingEmail) {
+    throw { code: "Unauthorized", message: "Incorrect e-mail and/or password" };
+  }
+  const checkPassword = bcrypt.compareSync(
+    userData.password,
+    existingEmail.password
+  );
+  if (!checkPassword) {
+    throw { code: "Unauthorized", message: "Incorrect e-mail and/or password" };
+  }
 
-    const secretKey:string | undefined = process.env.JWT_SECRET
-    const token:string = jwt.sign({id: existingEmail.id}, secretKey!)
-    const obj: {token: string}= {token}
-    return obj
+  const secretKey: string | undefined = process.env.JWT_SECRET;
+  const token: string = jwt.sign({ id: existingEmail.id }, secretKey!);
+  const obj: { token: string } = { token };
+  return obj;
 }
 
-export{
-    createUser,
-    signIn
+async function findById(id: number) {
+  const user = await userRepository.findById(id);
+  if (!user) {
+    throw { code: "NotFound", message: "User not Found" };
+  }
+  return user;
 }
+
+export { createUser, signIn, findById };
